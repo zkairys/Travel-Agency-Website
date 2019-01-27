02b97801ad4cbd7f07dfcd990c72ae9b98162388
Zygimantas Kairys <zygi@analogrepublic.com>
2019-01-27 17:44:19 +0000

    test commit

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export class ContactForm extends React.Component {
     state = {
-        name: " ",
-        email: " "
+        name: "",
+        email: ""
     }
     
 
